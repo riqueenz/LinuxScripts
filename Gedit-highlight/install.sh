8e35db9b7131c479108defa918bbc818e3af5d1a
Henrique Enzweiler <henriqueenzweiler@gmail.com>
2023-03-25 23:59:51 -0300

    solaris

--- Gedit-highlight/install.sh
+++ Gedit-highlight/install.sh
@@ -2,9 +2,9 @@
 echo "This script install a syntax highlight complement for gedit"
 dir_scope=/usr/share/gtksourceview-3.0/language-specs/
 echo "file copied"
-sudo \cp gcode.lang $dir_scope
-sudo \cp gcode.lang /usr/share/gtksourceview-4/language-specs/
-sudo \cp gcode.lang /usr/share/gtksourceview-5/language-specs/
+sudo \cp *.lang $dir_scope
+sudo \cp *.lang /usr/share/gtksourceview-4/language-specs/
+sudo \cp *.lang /usr/share/gtksourceview-5/language-specs/
 for dir in /home/*/; do
     # strip trailing slash
     homedir="${dir%/}"
